8518f737945c705438e1d13e2d9994dd13a73dae
Leyen Qian <leyenqian@gmail.com>
2020-10-05 00:39:41 -0600

    initial status of product

--- FoodStore/Product.java
+++ FoodStore/Product.java
@@ -1,5 +1,27 @@
 package FoodStore;
 
 public class Product {
-    
+    private Object item;
+    private float price;
+
+    public Product(Object item, float price) {
+        this.set_item(item);
+        this.set_price(price);
+    }
+
+    public void set_price(float price) {
+        this.price = price;
+    }
+
+    public float get_price() {
+        return this.price;
+    }
+
+    public void set_item(Object item) {
+        this.item = item;
+    }
+
+    public Object get_item() {
+        return this.item;
+    }
 }
