f832fe8ef1f185192cc452cc5ebd2ab191e922a5
Leyen Qian <leyenqian@gmail.com>
2020-10-12 19:39:32 -0600

    use helper function

--- Customer/Type/Business.java
+++ Customer/Type/Business.java
@@ -7,19 +7,12 @@ import FoodStore.FoodStore;
 public class Business extends Customer {
 
     public boolean buy_rolls(FoodStore food_store) {
-        try {
-            for (int i = 0; i < 2; i ++) {
-                this.products.add(this.buy_services(food_store, food_store.get_product(FoodFactory.TYPE_EGG_ROLL)));
-                this.products.add(this.buy_services(food_store, food_store.get_product(FoodFactory.TYPE_JELLY_ROLL)));
-                this.products.add(this.buy_services(food_store, food_store.get_product(FoodFactory.TYPE_PASTRY_ROLL)));
-                this.products.add(this.buy_services(food_store, food_store.get_product(FoodFactory.TYPE_SAUSAGE_ROLL)));
-                this.products.add(this.buy_services(food_store, food_store.get_product(FoodFactory.TYPE_SPRING_ROLL)));
-            }
-        } catch (Exception e) {
-            //  business customer will only take their order if it is filled exactly as requested
-            System.out.println(e.getMessage());
-            return food_store.get_last_error_code() == FoodStore.CODE_NO_INVENTORY ? false : true;
-        }
+
+        if ( !this.buy_rolls_helper(food_store, FoodFactory.TYPE_EGG_ROLL, 2) ) return false;
+        if ( !this.buy_rolls_helper(food_store, FoodFactory.TYPE_JELLY_ROLL, 2) ) return false;
+        if ( !this.buy_rolls_helper(food_store, FoodFactory.TYPE_PASTRY_ROLL, 2) ) return false;
+        if ( !this.buy_rolls_helper(food_store, FoodFactory.TYPE_SAUSAGE_ROLL, 2) ) return false;
+        if ( !this.buy_rolls_helper(food_store, FoodFactory.TYPE_SPRING_ROLL, 2) ) return false;
         
         return true;
     }
